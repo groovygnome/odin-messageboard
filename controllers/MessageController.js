b7e72a48d209d3276974ad18de41d6a11497b2fe
groovygnome <jrp810@proton.me>
2026-06-21 18:35:31 -0400

    start work on validation

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,11 +1,20 @@
 const messages = require('../models/messages');
+const { body, validationResult } = require('express-validator');
 
 module.exports = {
     get: (req, res) => {
         res.render('form');
     },
-    post: (req, res) => {
-        messages.push({ text: req.body.messageText, user: req.body.userName, added: new Date() });
-        res.redirect('/');
-    },
+    post: [
+        body('messageText').notEmpty().withMessage('Message is required'),
+        (req, res) => {
+            const errors = validationResult(req);
+            if (errors.isEmpty()) {
+                messages.push({ text: req.body.messageText, user: req.body.userName, added: new Date() });
+                res.redirect('/');
+            } else {
+                return res.status(400).render('index', { errors: errors.array(), messages: messages });
+            }
+        },
+    ],
 };
